54967249c0900a1909cd8d218579482ce1d4e46d
Praful Mahajan <79085151+Prafulm8o6@users.noreply.github.com>
2023-01-06 19:04:41 +0530

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserRegistrationDotNetCore.Models;
+
+namespace UserRegistrationDotNetCore.Data
+{
+    public class DataContext : IdentityDbContext<AppilcationUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
